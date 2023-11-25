1deb29244c96770aff3237131b14d94ab00b23e5
adalarf <kirilsloboda@gmail.com>
2023-11-25 17:01:04 +0500

    first commit

---
+++ privetproject/privet/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, AbstractBaseUser
+
+# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, university, **extra_fields):
+        if not email:
+            raise ValueError("The email is not given")
+        user = self.model(
+            email=self.normalize_email(email),
+            university=university,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, university, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError("Superuser must have is_staff = True")
+
+        if not extra_fields.get('is_superuser'):
+            raise ValueError("Superuser must have is_superuser = True")
+        return self.create_user(email, password, university, **extra_fields)
+
+class User(AbstractBaseUser):
+    university = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['university']
+
+    def __str__(self):
+        return self.email
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def has_perm(self, perm, obj=None):
+        return True
