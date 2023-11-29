11dd936c635f848a9a1d58db5fad3ac1ab1d4e60
adalarf <kirilsloboda@gmail.com>
2023-11-29 23:12:40 +0500

    adding student profile permission

--- privetproject/privet/views.py
+++ privetproject/privet/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, CreateAPIView
 from .models import User, UserInfo
 from .serializers import UserInfoEditSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 class UserInfoEditAPIView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserInfoEditSerializer
+    permission_classes = (IsAuthenticated, )
